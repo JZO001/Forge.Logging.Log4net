9e45889bd22d7abe7750bf14a85e6047920caba8
Zoltan Juhasz <jzo@jzo.hu>
2022-12-14 17:13:19 +0100

    Initial commit

---
+++ Log4NetLog.cs
@@ -0,0 +1,155 @@
+﻿using Forge.Logging.Abstraction;
+using Forge.Shared;
+using System;
+
+namespace Forge.Logging.Log4net
+{
+
+    /// <summary>Log wrapper implementation</summary>
+    /// <seealso cref="Abstraction.ILog" />
+    public class Log4NetLog : ILog
+    {
+
+        private readonly log4net.ILog mLog = null;
+
+        /// <summary>Initializes a new instance of the <see cref="Log4NetLog" /> class.</summary>
+        /// <param name="log">The log.</param>
+        public Log4NetLog(log4net.ILog log)
+        {
+            if (log == null) ThrowHelper.ThrowArgumentNullException("log");
+            mLog = log;
+        }
+
+        /// <summary>Gets a value indicating whether this instance is fatal enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is fatal enabled; otherwise, <c>false</c>.</value>
+        public bool IsFatalEnabled { get { return mLog.IsFatalEnabled; } }
+
+        /// <summary>Gets a value indicating whether this instance is warn enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is warn enabled; otherwise, <c>false</c>.</value>
+        public bool IsWarnEnabled { get { return mLog.IsWarnEnabled; } }
+
+        /// <summary>Gets a value indicating whether this instance is information enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is information enabled; otherwise, <c>false</c>.</value>
+        public bool IsInfoEnabled { get { return mLog.IsInfoEnabled; } }
+
+        /// <summary>Gets a value indicating whether this instance is debug enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is debug enabled; otherwise, <c>false</c>.</value>
+        public bool IsDebugEnabled { get { return mLog.IsDebugEnabled; } }
+
+        /// <summary>Gets a value indicating whether this instance is error enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is error enabled; otherwise, <c>false</c>.</value>
+        public bool IsErrorEnabled { get { return mLog.IsErrorEnabled; } }
+
+        /// <summary>Gets a value indicating whether this instance is trace enabled.</summary>
+        /// <value>
+        ///   <c>true</c> if this instance is trace enabled; otherwise, <c>false</c>.</value>
+        public bool IsTraceEnabled { get { return false; } }
+
+        /// <summary>Logs the specified message as a debug entry</summary>
+        /// <param name="message">The message.</param>
+        public void Debug(string message)
+        {
+            mLog.Debug(message);
+        }
+
+        /// <summary>Logs the specified message as a debug entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Debug(string message, Exception exception)
+        {
+            mLog.Debug(message, exception);
+        }
+
+        /// <summary>Logs the specified message as a error entry</summary>
+        /// <param name="message">The message.</param>
+        public void Error(string message)
+        {
+            mLog.Error(message);
+        }
+
+        /// <summary>Logs the specified message as a error entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Error(string message, Exception exception)
+        {
+            mLog.Error(message, exception);
+        }
+
+        /// <summary>Logs the specified message as a fatal/critical entry</summary>
+        /// <param name="message">The message.</param>
+        public void Fatal(string message)
+        {
+            mLog.Fatal(message);
+        }
+
+        /// <summary>Logs the specified message as a fatal/critical entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Fatal(string message, Exception exception)
+        {
+            mLog.Fatal(message, exception);
+        }
+
+        /// <summary>Logs the specified message as a info entry</summary>
+        /// <param name="message">The message.</param>
+        public void Info(string message)
+        {
+            mLog.Info(message);
+        }
+
+        /// <summary>Logs the specified message as a info entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Info(string message, Exception exception)
+        {
+            mLog.Info(message, exception);
+        }
+
+        /// <summary>Logs the specified message as a trace entry</summary>
+        /// <param name="message">The message.</param>
+        public void Trace(string message)
+        {
+        }
+
+        /// <summary>Logs the specified message as a trace entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Trace(string message, Exception exception)
+        {
+        }
+
+        /// <summary>Logs the specified message as a warning entry</summary>
+        /// <param name="message">The message.</param>
+        public void Warn(string message)
+        {
+            mLog.Warn(message);
+        }
+
+        /// <summary>Logs the specified message as a warning entry</summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        public void Warn(string message, Exception exception)
+        {
+            mLog.Warn(message, exception);
+        }
+
+    }
+
+    /// <summary>Log wrapper implementation</summary>
+    public class Log4NetLog<TLoggerType> : Log4NetLog, ILog<TLoggerType>
+    {
+
+        /// <summary>Initializes a new instance of the <see cref="Log4NetLog{TLoggerType}" /> class.</summary>
+        /// <param name="log">The log.</param>
+        public Log4NetLog(log4net.ILog log) : base(log)
+        {
+        }
+
+    }
+
+}
---
+++ Log4NetManager.cs
@@ -0,0 +1,151 @@
+﻿#if NETCOREAPP3_1_OR_GREATER
+using Forge.Logging.Abstraction;
+using Forge.Shared;
+#endif
+using Forge.Logging.Abstraction;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Forge.Logging.Log4net
+{
+
+    /// <summary>Log4Net wrapper implementation</summary>
+    /// <seealso cref="Abstraction.ILoggerWrapper" />
+    public class Log4NetManager : ILoggerWrapper
+    {
+
+        /// <summary>Initializes the <see cref="Log4NetManager" /> class.</summary>
+        static Log4NetManager()
+        {
+            Instance = new Log4NetManager();
+            SetLogger();
+        }
+
+        private Log4NetManager()
+        {
+        }
+
+        /// <summary>Gets the singleton instance.</summary>
+        /// <value>The instance.</value>
+        public static Log4NetManager Instance { get; private set; }
+
+        /// <summary>Initializes from application configuration.</summary>
+        public static void InitializeFromAppConfig()
+        {
+#if NETCOREAPP3_1_OR_GREATER
+            System.Xml.XmlDocument log4netConfig = new System.Xml.XmlDocument();
+            log4netConfig.Load(System.IO.File.OpenRead(System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.None).FilePath));
+            var repo = log4net. LogManager.CreateRepository(Assembly.GetEntryAssembly(),
+               typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(repo, (System.Xml.XmlElement)log4netConfig.GetElementsByTagName("log4net")[0]);
+#else
+            log4net.Config.XmlConfigurator.Configure();
+#endif
+            //SetLogger();
+        }
+
+#if NETCOREAPP3_1_OR_GREATER
+        /// <summary>Initializes from configuration file.</summary>
+        public static void InitializeFromConfigFile()
+        {
+            InitializeFromConfigFile(new FileInfo(Path.Combine(
+                PathHelper.CutoffLastEntryFromPath(System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.None).FilePath), 
+                "log4net.config")));
+        }
+#endif
+
+        /// <summary>Initializes from configuration file.</summary>
+        /// <param name="configFileFullPath">The configuration file full path.</param>
+        public static void InitializeFromConfigFile(string configFileFullPath)
+        {
+            InitializeFromConfigFile(new FileInfo(configFileFullPath));
+        }
+
+        /// <summary>Initializes from configuration file.</summary>
+        /// <param name="configFileInfo">The configuration file information.</param>
+        public static void InitializeFromConfigFile(FileInfo configFileInfo)
+        {
+            System.Xml.XmlDocument log4netConfig = new System.Xml.XmlDocument();
+            log4netConfig.Load(System.IO.File.OpenRead(configFileInfo.FullName));
+            var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(),
+               typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+            //SetLogger();
+        }
+
+        private static void SetLogger()
+        {
+            LogManager.LOGGER = Instance;
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public ILog GetLogger(string name)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(name));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="repository">The repository.</param>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public ILog GetLogger(string repository, string name)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(repository, name));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="repositoryAssembly">The repository assembly.</param>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public ILog GetLogger(Assembly repositoryAssembly, Type type)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(repositoryAssembly, type));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="repository">The repository.</param>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public ILog GetLogger(string repository, Type type)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(repository, type));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public ILog GetLogger(Type type)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(type));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <param name="repositoryAssembly">The repository assembly.</param>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public ILog GetLogger(Assembly repositoryAssembly, string name)
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger(repositoryAssembly, name));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <returns>ILog</returns>
+        public ILog GetLogger()
+        {
+            return new Log4NetLog(log4net.LogManager.GetLogger("[]"));
+        }
+
+        /// <summary>Gets the logger.</summary>
+        /// <typeparam name="TLoggerType">The type of the logger type.</typeparam>
+        /// <returns>ILog</returns>
+        public ILog<TLoggerType> GetLogger<TLoggerType>()
+        {
+            return new Log4NetLog<TLoggerType>(log4net.LogManager.GetLogger(typeof(TLoggerType)));
+        }
+
+    }
+
+}
